5a1209ec7b4c300ecff662d9cfcea91ffeede46b
a-dugonin <a_dugonin@mail.ru>
2024-02-01 18:15:10 +0300

    task_2: Реализован декоратор который замедляет время выполнения функций согласно задачи

--- decorators.py
+++ decorators.py
@@ -1,3 +1,4 @@
+import time
 from functools import wraps
 from typing import Callable
 
@@ -7,6 +8,7 @@ def how_are_you(func: Callable) -> Callable:
     Декоратор, который спрашивает как дела и сам же на этот вопрос и отвечает :)
     :param func: Callable :return: Callable
     """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         """ Функция обертка """
@@ -16,7 +18,24 @@ def how_are_you(func: Callable) -> Callable:
     return wrapper
 
 
+def code_slowdown(func: Callable) -> Callable:
+    """
+    Декоратор. Замедляет время выполнения декорируемой функции
+    :param func: Callable :return: Callable
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """ Функция обертка """
+        print('Немного терпения...')
+        time.sleep(2)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 if __name__ == '__main__':
+    @code_slowdown
     @how_are_you
     def test():
         print('<Тут что-то происходит...>')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import decorators
 
 
+@decorators.code_slowdown
 @decorators.how_are_you
 def test():
     print('<Тут что-то происходит...>')
