44411f8d590536b9e136fddf30dcad8d91f81f3e
a-dugonin <a_dugonin@mail.ru>
2024-02-01 17:58:52 +0300

    task_1: Реализована функия декоратор согласно задачи. Задача описана в readme файле

---
+++ decorators.py
@@ -0,0 +1,25 @@
+from functools import wraps
+from typing import Callable
+
+
+def how_are_you(func: Callable) -> Callable:
+    """
+    Декоратор, который спрашивает как дела и сам же на этот вопрос и отвечает :)
+    :param func: Callable :return: Callable
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """ Функция обертка """
+        print('Как дела? Хорошо.\nА у меня не очень! Ладно, держи свою функцию.')
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+if __name__ == '__main__':
+    @how_are_you
+    def test():
+        print('<Тут что-то происходит...>')
+
+
+    test()
---
+++ main.py
@@ -0,0 +1,9 @@
+import decorators
+
+
+@decorators.how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
