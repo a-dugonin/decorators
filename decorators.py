b92acf6576e0a6c8da1f491b7b0da276c22406e0
a-dugonin <a_dugonin@mail.ru>
2024-02-02 12:11:45 +0300

    task_3: Реализован декоратор который производит логирование декорируемой функции

--- decorators.py
+++ decorators.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 from functools import wraps
 from typing import Callable
@@ -5,7 +6,7 @@ from typing import Callable
 
 def how_are_you(func: Callable) -> Callable:
     """
-    Декоратор, который спрашивает как дела и сам же на этот вопрос и отвечает :)
+    Декоратор, который спрашивает как дела и сам же на этот вопрос и отвечает:)
     :param func: Callable :return: Callable
     """
 
@@ -34,11 +35,34 @@ def code_slowdown(func: Callable) -> Callable:
     return wrapper
 
 
+def logging(func: Callable) -> Callable:
+    """
+    Декоратор. Осуществляет логирование выполнения функций. Записывает ошибки возникающие в ходе выполнения
+    декорируемой функции
+    :param func: Callable :return: Callable
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """ Функция обертка """
+        print(func.__name__, func.__doc__, sep='\n')
+        try:
+            func(*args, **kwargs)
+        except Exception as error:
+            with open('function_errors.log', 'a', encoding='utf-8') as file:
+                file.write(f"\n{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - в ходе выполнения функции"
+                           f" {func.__name__} произошла ошибка - {error}")
+
+    return wrapper
+
+
 if __name__ == '__main__':
+    @logging
     @code_slowdown
     @how_are_you
     def test():
-        print('<Тут что-то происходит...>')
+        """ Функция для тестирования декораторов """
+        # print('<Тут что-то происходит...>')
+        print(12/0)
 
 
     test()
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import decorators
 
 
+@decorators.logging
 @decorators.code_slowdown
 @decorators.how_are_you
 def test():
+    """ Функция для тестирования декораторов """
     print('<Тут что-то происходит...>')
+    print(1/0)
 
 
 test()
